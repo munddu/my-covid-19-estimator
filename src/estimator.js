1305122a5bdb2ce86449d533195600b2b4f9c43c
munddu <ssemagoyeumar@gmail.com>
2020-04-12 22:37:28 -0700

    This is my first step for test

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,10 @@
 const covid19ImpactEstimator = (data) => data;
 
 export default covid19ImpactEstimator;
+
+data: {};
+impact: {};
+severeImpact: {};
+
+let currentlyInfected = reportedCases * 10;
+print("the number of currently infected people is " + impact.currentlyInfected);
