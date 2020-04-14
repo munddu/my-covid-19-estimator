eb8ab0c4463093b5f49ae1036b9902a74faada49
munddu <ssemagoyeumar@gmail.com>
2020-04-14 11:15:22 -0700

    Testing the modules

--- src/estimator.js
+++ src/estimator.js
@@ -1,30 +1,30 @@
 const covid19ImpactEstimator = (data) => data;
 
-//data: {};
-impact: {};
-severeImpact: {};
-//for impact
+//  data: {};
+//  impact: {};
+//  severeImpact: {};
+//  for impact
 var currentlyInfected = reportedCases * 10;
 console.log(impact.currentlyInfected);
 
 var infectionsByRequestedTime = currentlyInfected* 512;
 
-//For severe impact
+//  For severe impact
 var currentlyInfected = reportedCases * 50;
 console.log(severeImpact.currentlyInfected);
 
 var infectionsByRequestedTime = currentlyInfected * 512;
 
-//challenge 2
+//  challenge 2
 var severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime;
 var hospitalBedsByRequestedTime = (0.35 * totalHospitalBeds)/severeCasesByRequestedTime;
 console.log(hospitalBedsByRequestedTime);
 
-//challenge 3
+//  challenge 3
 var casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime;
 console.log(casesForICUByRequestedTime);
 var casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime;
-window.alert(casesForVentilatorsByRequestedTime);
+console.log(casesForVentilatorsByRequestedTime);
 
 var dollarsInFlight = (infectionsByRequestedTime * 0.65 * 1.5) / 30;
 console.log(dollarsInFlight);
